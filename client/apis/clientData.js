1d0c2bbca489202d409b22826fe72e9db14154ad
Grant Scott-Hayes <107601640+grant-scott-hayes@users.noreply.github.com>
2022-09-19 17:30:03 +1200

    get api working

--- client/apis/clientData.js
+++ client/apis/clientData.js
@@ -4,6 +4,7 @@ const rootUrl = '/api/v1'
 
 export function getClientData() {
   return request.get(rootUrl + '/clientData').then((res) => {
-    return res.body.clientData
+    //console.log(res.body)
+    return res.body
   })
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Fruits from './Fruits'
 import ClientForm from './ClientForm'
+import { getClientData } from '../apis/clientData'
 
 const App = () => {
+  useEffect(() => {
+    getClientData()
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }, [])
+
   return (
     <div className="flex flex-col  items-center bg-sky-200 h-screen w-screen">
       <h1 className="text-3xl font-bold text-neutral-800 py-6">
